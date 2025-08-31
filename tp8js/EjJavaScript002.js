9cba5902e63d721423c0e432ec6794c0e3067f63
elu-rique1 <elugutierrezriquelme9@gmail.com>
2025-08-30 21:17:25 -0300

    Add files via upload

---
+++ tp8js/EjJavaScript002.js
@@ -0,0 +1,9 @@
+function woas(){ 
+    alert("Datos pernales");
+    alert("Eluney");
+    alert("Riquelme");
+    alert("Soltero");
+    alert("18");
+    alert("09/07/2007");
+    alert("47929960");
+}
